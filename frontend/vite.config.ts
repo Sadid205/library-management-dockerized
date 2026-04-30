b0cb16d26a6c45e745925e5e99714d907135fe22
Md.Abdullah Al Sadid <abdullahalsadid1914@gmail.com>
2026-04-30 08:11:44 +0600

    CICD Pipeline has been running

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,10 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
-const allowedHosts = import.meta.env.VITE_ALLOWED_HOSTS.split(",");
+
+const allowedHosts = process.env.VITE_ALLOWED_HOSTS
+  ? process.env.VITE_ALLOWED_HOSTS.split(",")
+  : [];
 
 // https://vite.dev/config/
 export default defineConfig({

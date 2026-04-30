e68b5316189e8acfd36603e83635ab4be55c63e9
Md.Abdullah Al Sadid <abdullahalsadid1914@gmail.com>
2026-04-30 08:52:07 +0600

    modified vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,25 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-const allowedHosts = process.env.VITE_ALLOWED_HOSTS
-  ? process.env.VITE_ALLOWED_HOSTS.split(",")
-  : [];
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const allowedHosts = env.VITE_ALLOWED_HOSTS
+    ? env.VITE_ALLOWED_HOSTS.split(",")
+    : [];
+  const port = env.PORT ? parseInt(env.PORT) : 4173;
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  preview: {
-    allowedHosts: allowedHosts,
-  },
+    preview: {
+      allowedHosts: allowedHosts,
+      host: true,
+      port: port,
+    },
+  };
 });
